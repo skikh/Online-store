01b64e188ac0f3c6039804722345f36107f454a6
Aleksandr Nikitinskikh <ya-kaze-san@yandex.ru>
2022-09-14 10:12:36 +0500

    Start to make script for sub-catalog

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,27 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.closest('[data-parent]')) {
+        const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+        
+        const subMenu = document.querySelector('[data-subcatalog="$(subMenuId)"]');
+
+        if (subMenu){
+            subMenu.classList.add('_sub-catalog-open');
+        } else{
+          console.log('Ой, что-то пошло не так')
+        }
+        e.preventDefault();
+    }
+}
+
+
+
+
+
