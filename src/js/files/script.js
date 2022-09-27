2c90ea23f1a5321257f5d5f8ec7776f49b0d44dd
Aleksandr Nikitinskikh <ya-kaze-san@yandex.ru>
2022-09-27 12:53:03 +0500

    Try to fix bug wtuh menu-humburger

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,7 +1,7 @@
-
-import { isMobile } from "./functions.js";
+// Подключение функционала "Чертогов Фрилансера"
+// import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+// import { flsModules } from "./modules.js";
 
 
 document.addEventListener("click", documentActions);
@@ -38,9 +38,33 @@ function documentActions(e) {
     }
     e.preventDefault();
   }
+
+  if (targetElement.closest('.menu-top-header__link_catalog')) {
+    document.documentElement.classList.add('catalog-open');
+    e.preventDefault();
+  }
+
+  if (targetElement.closest('.menu-catalog__back')) {
+    document.documentElement.classList.remove('catalog-open');
+
+    document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+    document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+    e.preventDefault();
+  }
+
+  if (targetElement.closest('.sub-menu-catalog__back')) {
+    document.documentElement.classList.remove('sub-menu-open');
+    document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+    document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+    e.preventDefault();
+  }
+
 }
 
-
-
-
-
+if (document.querySelector('.filter-catalog__title')) {
+  document.querySelector('.filter-catalog__title').addEventListener('click', () => {
+    if (window.innerWidth < 992) {
+      document.querySelector('.filter-catalog__items').classList.toggle('_active');
+    }
+  });
+}
